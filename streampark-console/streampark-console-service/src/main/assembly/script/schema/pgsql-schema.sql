801eccf8566ba18d9c63cdb109b04622fb2f3618
Roc Marshal <flinker@126.com>
2023-02-23 13:10:58 +0800

    [Feature][Issue-2324] Introduce yarn queue setting to manage the queue resouce.
    Add DDL & DML statements for SQL.

--- streampark-console/streampark-console-service/src/main/assembly/script/schema/pgsql-schema.sql
+++ streampark-console/streampark-console-service/src/main/assembly/script/schema/pgsql-schema.sql
@@ -42,6 +42,7 @@ drop table if exists "public"."t_access_token";
 drop table if exists "public"."t_flink_log";
 drop table if exists "public"."t_team";
 drop table if exists "public"."t_variable";
+drop table if exists "public"."t_yarn_queue";
 
 -- ----------------------------
 -- drop sequence if exists
@@ -66,6 +67,7 @@ drop sequence if exists "public"."streampark_t_access_token_id_seq";
 drop sequence if exists "public"."streampark_t_flink_log_id_seq";
 drop sequence if exists "public"."streampark_t_team_id_seq";
 drop sequence if exists "public"."streampark_t_variable_id_seq";
+drop sequence if exists "public"."streampark_t_yarn_queue_id_seq";
 
 -- ----------------------------
 -- drop trigger if exists
@@ -740,6 +742,31 @@ create index "un_user_role_inx" on "public"."t_member" using btree (
   "user_id" "pg_catalog"."int8_ops" asc nulls last,
   "role_id" "pg_catalog"."int8_ops" asc nulls last
 );
+
+-- ----------------------------
+-- table structure for t_yarn_queue
+-- ----------------------------
+create sequence "public"."streampark_t_yarn_queue_id_seq"
+    increment 1 start 10000 cache 1 minvalue 10000 maxvalue 9223372036854775807;
+
+create table "public"."t_yarn_queue" (
+  "id" int8 not null default nextval('streampark_t_yarn_queue_id_seq'::regclass),
+  `team_id` int8 not null comment 'team id',
+  "queue_label" varchar(255) not null collate "pg_catalog"."default",
+  "description" varchar(512) collate "pg_catalog"."default",
+  "create_time" timestamp(6) not null default timezone('UTC-8'::text, (now())::timestamp(0) without time zone),
+  "modify_time" timestamp(6) not null default timezone('UTC-8'::text, (now())::timestamp(0) without time zone)
+)
+;
+comment on column "public"."t_yarn_queue"."id" is 'queue id';
+comment on column "public"."t_yarn_queue"."team_id" is 'team id';
+comment on column "public"."t_yarn_queue"."queue_label" is 'queue label expression';
+comment on column "public"."t_yarn_queue"."description" is 'description of the queue';
+comment on column "public"."t_yarn_queue"."create_time" is 'create time';
+comment on column "public"."t_yarn_queue"."modify_time" is 'modify time';
+
+alter table "public"."t_yarn_queue" add constraint "t_yarn_queue_pkey" primary key ("id");
+alter table "public"."t_yarn_queue" add constraint "unique_team_id_queue_label" unique("team_id", "queue_label");
 
 -- -----------------------------------------
 -- trigger for table with modify_time field
--- streampark-console/streampark-console-service/src/main/assembly/script/upgrade/mysql/2.1.0.sql
+++ streampark-console/streampark-console-service/src/main/assembly/script/upgrade/mysql/2.1.0.sql
@@ -28,3 +28,25 @@ insert into `t_menu` values (100070, 100015, 'savepoint trigger', null, null, 's
 alter table `t_flink_app` change column `launch` `release` tinyint default 1;
 update `t_menu` set `menu_name`='release',`perms` = 'app:release' where `menu_id` = 100025;
 -- ISSUE-2366 DDL & DML End
+
+
+-- Issue-2324 Start --
+
+insert into `t_menu` values (100071, 100033, 'add yarn queue', null, null, 'yarnQueue:create', '', '1', 0, null, now(), now());
+insert into `t_menu` values (100072, 100033, 'edit yarn queue', null, null, 'yarnQueue:update', '', '1', 0, null, now(), now());
+
+-- ----------------------------
+-- table structure for t_yarn_queue
+-- ----------------------------
+drop table if exists `t_yarn_queue`;
+create table `t_yarn_queue` (
+  `id` bigint not null primary key auto_increment comment 'queue id',
+  `team_id` bigint not null comment 'team id',
+  `queue_label` varchar(255) collate utf8mb4_general_ci not null comment 'queue and label expression',
+  `description` varchar(512) collate utf8mb4_general_ci default null comment 'description of the queue label',
+  `create_time` datetime not null default current_timestamp comment 'create time',
+  `modify_time` datetime not null default current_timestamp on update current_timestamp comment 'modify time',
+   unique key `unq_team_id_queue_label` (`team_id`, `queue_label`) using btree
+) engine = innodb default charset = utf8mb4 collate = utf8mb4_general_ci;
+
+-- Issue-2324 End --
